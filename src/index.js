e515a5cc7389ef0981932bd65574b7c55d2d39b1
Jesse <jessemchung1@gmail.com>
2021-08-28 15:24:03 -0700

    basic1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+require("./styles.css")
+class App extends React.Component {
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
+var mountNode = document.getElementById("app");
+ReactDOM.render(<App name="world" />, mountNode);
